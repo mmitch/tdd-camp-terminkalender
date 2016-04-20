9127090ccde5dfa2438ae33bde7183ce95454edc
Christian Garbs <mitch@cgarbs.de>
2016-04-20 23:07:01 +0200

    Log bei Termineintrag - grüner Pfad
    setter-Injection:
    - beschränkter Zugriff (package-Scope), reicht für Unit-Test
    - Default-Wert, damit der Setter im Normalbetrieb nicht aufgerufen werden muss

--- src/main/java/com/itagile/tddcamp/terminkalender/Kalender.java
+++ src/main/java/com/itagile/tddcamp/terminkalender/Kalender.java
@@ -3,15 +3,19 @@ package com.itagile.tddcamp.terminkalender;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.itagile.tddcamp.log.FileLogger;
+
 public class Kalender {
-	
+
 	List<Termin> termine = new ArrayList<Termin>();
 	private Teilnehmer teilnehmer;
+	private Logger logger = new FileLogger();
 
 	public void trageEin(Termin termin) {
 		if(hat(termin)) throw new TerminDoppeltEingetragenException();
 		termin.ladeEin(teilnehmer);
 		termine.add(termin);
+		logger.neuerTerminEingetragen(this, termin);
 	}
 
 	public boolean hat(Termin termin) {
@@ -20,5 +24,9 @@ public class Kalender {
 
 	public void setBesitzer(Teilnehmer teilnehmer) {
 		this.teilnehmer = teilnehmer;
+	}
+
+	void setLogger(Logger logger) {
+		this.logger = logger;
 	}
 }
